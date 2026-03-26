9f7bbd5335c7f1df863f890159a23967ab7734e9
SkNightmare <SkNightmare@jcc74.com>
2026-03-26 01:39:12 -0400

    android tauri init

---
+++ src-tauri/gen/android/app/src/main/java/com/jcc74/jcirc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jcc74.jcirc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
